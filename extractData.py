8af381dc29fee6fe28ed921436bb1b1b6e0a4d73
rafi-doelandri <rafidoelandri28@gmail.com>
2026-08-06 16:51:57 +0700

    fix: fix commented code

--- extractData.py
+++ extractData.py
@@ -7,25 +7,24 @@ from dotenv import load_dotenv
 load_dotenv()
 url: str = os.getenv("API_URL")
 key: dict = {"key": os.getenv("API_KEY")}
-print(f"data type: {type(key)}")
 
-# r = []
+r = []
 
-# with requests.Session() as session:
-#     for i in range(20):
-#         try:
-#             response = session.get(url, params=key, timeout=5).json()
-#             r.append(response)
-#             print(f"Request ke-{i+1}: Data {response['source_ip']}")
+with requests.Session() as session:
+    for i in range(20):
+        try:
+            response = session.get(url, params=key, timeout=5).json()
+            r.append(response)
+            print(f"Request ke-{i+1}: Data {response['source_ip']}")
 
-#         except requests.exceptions.RequestException as e:
-#             print(f"Request ke-{i+1} Gagal: {e}")
+        except requests.exceptions.RequestException as e:
+            print(f"Request ke-{i+1} Gagal: {e}")
 
-#         time.sleep(1)
+        time.sleep(1)
 
-# if os.path.exists("network_log.json"):
-#     with open("network_log.json", "a", encoding="utf-8") as file:
-#         file.write(json.dumps(r, indent=4))
-# else:
-#     with open("network_log.json", "w", encoding="utf-8") as file:
-#         json.dump(r, file, indent=4)
+if os.path.exists("network_log.json"):
+    with open("network_log.json", "a", encoding="utf-8") as file:
+        file.write(json.dumps(r, indent=4))
+else:
+    with open("network_log.json", "w", encoding="utf-8") as file:
+        json.dump(r, file, indent=4)
